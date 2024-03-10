9b07cca4b81a8b1ca25773339eb7f2c64f6f0daf
Ramesh S <rameshsudhan11@gmail.com>
2024-03-10 09:42:06 +0530

    first commit

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+# Creating an EC2 instance
+/*resource "aws_instance" "web" {
+  ami           = "ami-07d9b9ddc6cd8dd30"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Test_ins"
+  }
+}*/
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA6ODU2CRGHYQ6FZTX"
+    secret_key = "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"
+}
